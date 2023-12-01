00c6edcb5aeeea325bc6e6885b8ee6188e76358f
lisa-vsqz <liseth.abad@udla.edu.ec>
2023-12-01 01:00:52 -0500

    Correcciones en clase Login

---
+++ Pages/Login.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorApp.Pages
+{
+    public class Login : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+
+
+
+    }
+}
